a5d1ef21205ff48ec4682cfe317b000f9eefbd62
Sechaba Motaung <sechabamot@gmail.com>
2021-11-08 09:51:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpInterviewCodeQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
