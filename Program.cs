aabb8dc21ddf06c88ab3a4d971b945a7ae3ecdc8
Sechaba Motaung <sechabamot@gmail.com>
2021-11-26 19:07:24 +0200

    Questions 6-7

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CsharpInterviewCodeQuestions
 {
@@ -17,8 +18,13 @@ namespace CsharpInterviewCodeQuestions
             Console.WriteLine($"This sentence '{sentenceToReverse}' read backwords is: {ReverseTheSentence(sentenceToReverse)}");
             //Q4
             int[] intArray = new int[] {2,4,6,5};
-            Console.WriteLine($"The total of the even numbers in {intArray.ToString()} {ReturnTotalOfEvenNumbers(intArray)}");
-
+            Console.WriteLine($"The total of the even numbers in [2,4,6,5] array are: {ReturnTotalOfEvenNumbers(intArray)}");
+            //Q5
+            string countCharacters = "Count these characters";
+            Console.WriteLine($"Total character count in [ {countCharacters} ] : {CountOccurrences(countCharacters)}");
+            //Q6
+            string removeDuplicates = "aaaabbbbccccddddeeeeffff";
+            Console.WriteLine($"After removing duplicates in  [{removeDuplicates}]  we get : {RemoveDuplicateChar(removeDuplicates)}");
         }
 
 
@@ -85,7 +91,49 @@ namespace CsharpInterviewCodeQuestions
             return output;
         }
 
-        //Q5: 
+        //Q5: How to count the occurrence of each character in a string?
+        public static string CountOccurrences(string input)
+        {
+            Dictionary<char, int> characterCount = new Dictionary<char, int>();
+            string output = "";
 
+            foreach (var character in input.ToLower())
+            {
+                
+                if (!char.IsWhiteSpace(character))
+                {
+                    if (!characterCount.ContainsKey(character))
+                    {
+                        characterCount.Add(character, 1);
+                    }
+                    else
+                    {
+                        characterCount[character]++;
+                    }
+                }
+            }
+
+            foreach (var character in characterCount)
+            {
+                output  += $"[ {character.Key} : {character.Value}] ";
+                //Console.WriteLine("{0} - {1}", character.Key, character.Value);
+            }
+
+            return output;
+        }
+
+        //Q6: How to remove duplicate characters from a string?
+        public static string RemoveDuplicateChar(string input)
+        {
+            string output = "";
+            foreach (char c in input)
+            {
+                if (!output.Contains(c))
+                {
+                    output += c;
+                }
+            }
+            return output;
+        }
     }
 }
